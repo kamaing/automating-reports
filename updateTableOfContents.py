134ef249e60f26027e707ca138b3e8c653182938
K4114Ing <37227939+kamaing@users.noreply.github.com>
2019-07-14 01:31:50 +0200

    Update updateTableOfContents.py

--- updateTableOfContents.py
+++ updateTableOfContents.py
@@ -1,8 +1,10 @@
 import sys
+import os # for getting the current path
 from win32com.client import constants
 import win32com.client
 
 word = win32com.client.DispatchEx("Word.Application")
+cwd = os.getcwd() # get current path
 doc = word.Documents.Open('C:\\Users\\KamaIng\\real.docx')
 doc.TablesOfContents(1).Update()
 doc.Close(SaveChanges=True)
